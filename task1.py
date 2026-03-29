5d760f21d9ebaafffa20a43fe4a184efb31d1245
anzeljc7 <tilen.anzeljc7@gmail.com>
2026-03-29 20:55:20 +0200

    Cleanup

--- task1.py
+++ task1.py
@@ -11,22 +11,18 @@ from gem5.resources.resource import CustomResource  # type: ignore
 current_folder = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_folder, 'default'))
 
-# Zdaj ju lahko varno uvozimo
 from cpuInORD_model import RiscV_InOrder_CPU # type: ignore
 from cpuO3_model import RISCV_O3_CPU # type: ignore
-# --------------------------------
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--cpu", choices=["minor", "o3"], required=True)
 args = parser.parse_args()
 
-# Izbira procesorja
 processor = RiscV_InOrder_CPU() if args.cpu == "minor" else RISCV_O3_CPU()
 
 cache_hierarchy = PrivateL1CacheHierarchy(l1d_size="32KiB", l1i_size="32KiB")
 memory = SingleChannelDDR3_1600("7GiB")
 
-# Sestava računalnika (Board)
 board = SimpleBoard(
     clk_freq="3GHz",
     processor=processor,
@@ -34,12 +30,10 @@ board = SimpleBoard(
     cache_hierarchy=cache_hierarchy
 )
 
-# 3. Z absolutno potjo določimo program, ki se bo izvajal (znotraj mape workload)
 pot_do_binarne = os.path.join(current_folder, "workload", "scaled_dot_product_adv.bin")
 binary = CustomResource(pot_do_binarne)
 
 board.set_se_binary_workload(binary)
 
-# Priprava in zagon simulatorja
 simulator = Simulator(board=board)
 simulator.run()
